5c018a686718317d56df824ed04c11b2feea70ec
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2024-08-10 20:05:28 -0300

    initial commit

---
+++ src/main/java/com/github/guilhermebauer/studymanagement/StudymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.guilhermebauer.studymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/guilhermebauer/studymanagement/StudymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guilhermebauer.studymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
